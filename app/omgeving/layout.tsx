51ccf67102454083746714fdf4ad02cd59126c99
barteleveld <bart.eleveld@gmail.com>
2026-07-05 18:44:23 +0200

    Voeg productieomgevingen toe

---
+++ app/omgeving/layout.tsx
@@ -0,0 +1,7 @@
+import { DemoShell } from "@/components/layout/demo-shell";
+import { DemoStateProvider } from "@/components/demo/demo-state";
+
+export default function EnvironmentLayout({ children }: { children: React.ReactNode }) {
+  return <DemoStateProvider><DemoShell>{children}</DemoShell></DemoStateProvider>;
+}
+
