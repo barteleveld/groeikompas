a585865b3df5c4d1d7cb809393ac1b779e712238
barteleveld <bart.eleveld@gmail.com>
2026-07-06 12:36:20 +0200

    Beveilig echte omgevingen met rollen

--- app/omgeving/page.tsx
+++ app/omgeving/page.tsx
@@ -1,2 +1,8 @@
-export { default } from "@/app/demo/page";
+import { redirect } from "next/navigation";
+import { getCurrentProfile, roleHome } from "@/lib/auth/session";
 
+export default async function EnvironmentPage() {
+  const profile = await getCurrentProfile();
+  if (!profile) redirect("/login");
+  redirect(roleHome(profile.role));
+}
